ee1909012e65cf7a13d9e5597aad7ac11d9aba88
Iván Silvero <ivannsilvero@gmail.com>
2020-06-30 13:24:00 -0300

    docs agregado

---
+++ docs/precache-manifest.886e79d4e83eb27f2a4b64b165f91f02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d40344b400a926f434fd93ae7aa9c30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7f0de9bb3bd37a71e9",
+    "url": "/static/css/main.a65effae.chunk.css"
+  },
+  {
+    "revision": "2142ce882b7ad8d2e780",
+    "url": "/static/js/2.f82784ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f82784ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac7f0de9bb3bd37a71e9",
+    "url": "/static/js/main.1a4f2136.chunk.js"
+  },
+  {
+    "revision": "efc8d4bc350e30b3cd92",
+    "url": "/static/js/runtime-main.4252b490.js"
+  }
+]);
